4521f541ddcf29305e2ea034cc16b03feea3a392
Mykhailo Gogol <68640759+Mykhailo-Gogol@users.noreply.github.com>
2023-02-03 20:17:29 +0200

    basic navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
+import { FontAwesome } from '@expo/vector-icons'
+
+import Photos from './src/pages/Photos'
+import Favorites from './src/pages/Favorites'
+
+const Stack = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    // @ts-ignore
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={Photos}
+          options={{
+            tabBarLabel: 'Photos',
+            tabBarIcon: () => (
+              <FontAwesome name="home" size={24} color="black" />
+            ),
+          }}
+        />
+        <Stack.Screen
+          name="Saved"
+          component={Favorites}
+          options={{
+            tabBarLabel: 'Favorites',
+            tabBarIcon: () => (
+              <FontAwesome name="bookmark" size={24} color="black" />
+            ),
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/pages/Favorites.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Favorites() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+})
